8db10bd144002f0d6637be5fa85f46d065183acf
siercks <108698268+siercks@users.noreply.github.com>
2025-06-22 13:48:53 -0500

    250622

--- Scenes/Game/Game.cs
+++ Scenes/Game/Game.cs
@@ -3,19 +3,34 @@ using System;
 
 public partial class Game : Node2D
 {
-	[Export] private Fuel _fuel;
-	//[Signal] public delegate void OnFueledEventHandler();
+    [Export] private Fuel _fuel;
+	[Export] private PackedScene _fuelScene;
+	//[Export] private NodePath _fuelPath; // Just doing this to future-proof code.
+    //private Fuel _fuel;
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		//Fuel fuel = GetNode<Fuel>("Fuel");
+		//_fuel = GetNode<Fuel>(_fuelPath);
 		_fuel.OnFueled += OnFueled;
+        CreateFuel();
+
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+	{
+
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	private void CreateFuel()
 	{
+		Fuel fuel = (Fuel)_fuelScene.Instantiate();
+		AddChild(fuel);
+		fuel.Position = new Vector2(400, -100);
 	}
+
 	private void OnFueled()
 	{
 		GD.Print("OnFueled Received");
